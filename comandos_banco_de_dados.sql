931ccc767bc9addf2e1227101cf415ed11adbfd2
jose-rf <joserfranca13@gmail.com>
2024-12-14 13:37:37 -0300

    Comandos para criação do BD

---
+++ comandos_banco_de_dados.sql
@@ -0,0 +1,22 @@
+CREATE DATABASE cadastro;
+
+USE cadastro;
+
+CREATE TABLE cadastro.cadastro_pessoa (
+	id INT auto_increment NOT NULL,
+	nome varchar(100) NOT NULL,
+    endereco varchar(100) NOT NULL,
+    email varchar(100) NOT NULL,
+    telefone varchar(100) NOT NULL,
+    CONSTRAINT cadastro_pessoa_pk PRIMARY KEY (id)
+);
+
+select * from cadastro.cadastro_pessoa;
+
+CREATE TABLE cadastro.uf (
+	id INT auto_increment NOT NULL,
+	nome varchar(100) NOT NULL,
+    sigla varchar(2) NOT NULL,
+    CONSTRAINT uf_pk PRIMARY KEY (id)
+);
+
